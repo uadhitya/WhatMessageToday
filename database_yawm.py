54e5b3bd731da3138b05b8e390fb26e750e527c0
Aditya Purnama <38023797+uadhitya@users.noreply.github.com>
2026-03-04 04:32:24 +0700

    database_yawm.py

---
+++ database_yawm.py
@@ -0,0 +1,7 @@
+# Database 365 Kata 'Yawm' (Tunggal) dalam Al-Qur'an
+DATA_YAWM = {
+    1: ("Al-Fatihah", 1, 4, "Maaliki Yawmi-d-Diin"),
+    2: ("Al-Baqarah", 2, 8, "Bi-l-Yawmi-l-Aakhir"),
+    3: ("Al-Baqarah", 2, 48, "Yawman laa tajzii nafsun"),
+    # ... Anda bisa terus menambahkan hingga 365
+}
